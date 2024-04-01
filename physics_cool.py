4f0c86e741db0e5c88249533bc435871432c7a9c
pavlen5kiy <150275009+pavlen5kiy@users.noreply.github.com>
2024-04-01 21:34:52 +0300

    s

---
+++ physics_cool.py
@@ -0,0 +1,85 @@
+import pygame
+import pymunk
+import random
+
+
+def create_circle(space, pos, color):
+    body = pymunk.Body(1, 100, body_type=pymunk.Body.DYNAMIC)
+    body.position = pos
+    shape = pymunk.Circle(body, 10)
+    shape.elasticity = 1
+    shape.friction = 1000
+    space.add(body, shape)
+    return shape, color
+
+
+def draw_circles(circles):
+    for circle, color in circles:
+        x = int(circle.body.position.x)
+        y = int(circle.body.position.y)
+        pygame.draw.circle(screen, color, (x, y), 10)
+
+
+def create_static_ball(space):
+    body = pymunk.Body(1, 100, body_type=pymunk.Body.STATIC)
+    body.position = (450, 400)
+    shape = pymunk.Circle(body, 50)
+    shape.elasticity = 0.2
+    shape.friction = 2
+    space.add(body, shape)
+    return shape
+
+
+def draw_static_balls(balls):
+    for ball in balls:
+        x = int(ball.body.position.x)
+        y = int(ball.body.position.y)
+        pygame.draw.circle(screen, 'black', (x, y), 50)
+
+
+def create_boundaries(space, width, height):
+    rects = [
+        [(width / 2, height), (width, 0)],
+        [(width / 2, 0), (width, 0)],
+        [(0, height / 2), (0, height)],
+        [(width, height / 2), (0, height)]
+    ]
+
+    for pos, size in rects:
+        body = pymunk.Body(body_type=pymunk.Body.STATIC)
+        body.position = pos
+        shape = pymunk.Poly.create_box(body, size)
+        shape.elasticity = 1
+        shape.friction = 1000
+        space.add(body, shape)
+
+
+pygame.init()
+screen = pygame.display.set_mode((1080, 720))
+clock = pygame.time.Clock()
+space = pymunk.Space()
+space.gravity = (0, 1000)
+
+fps = 240
+circles = []
+balls = [create_static_ball(space)]
+create_boundaries(space, 1080, 720)
+
+running = True
+while running:
+    clock.tick(fps)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+        if event.type == pygame.MOUSEMOTION:
+            color = (random.choice(range(256)),
+                     random.choice(range(256)),
+                     random.choice(range(256)))
+            circles.append(create_circle(space, event.pos, color))
+
+    screen.fill('white')
+    draw_circles(circles)
+    draw_static_balls(balls)
+    space.step(1 / fps / 2)
+    pygame.display.flip()
